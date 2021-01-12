402c6d656982339b76397236c83ea34c68330c29
Anand014 <kumar.anand014@gmail.com>
2021-01-12 17:13:44 +0530

    working form

--- src/Components/Form/NewForm.js
+++ src/Components/Form/NewForm.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Form.css";
-import { Form, Field, useFormik } from "formik";
+import { useFormik } from "formik";
 import * as Yup from "yup";
 
 const NewForm = () => {
@@ -10,59 +10,78 @@ const NewForm = () => {
       email: "",
       password: "",
       currentJob: "",
-      image: "",
       description: "",
     },
   });
 
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log(formik.values);
+  };
+
   return (
     <div id="signinfrom" className="flex justify-center items-center">
-      <div className="glasseffect">
-        <form className="flex flex-col leading-8 m-5">
-          <h1 className="text-4xl my-2">Register</h1>
-          <label className="mx-5" htmlFor="name">
+      <div className="glasseffect flex justify-center items-center">
+        <form className="flex flex-col leading-8 m-5" onSubmit={submitHandler}>
+          <h1 className="text-2xl flex justify-center my-2  text-white">
+            Register
+          </h1>
+          <label className="text-white" htmlFor="name">
             Name
           </label>
           <input
-            className="w-60 rounded-full shadow-md"
+            className="w-80 rounded-lg shadow-md"
             type="text"
             name="name"
+            onChange={formik.handleChange}
+            value={formik.values.name}
           ></input>
-          <label className="mx-5" htmlFor="email">
+          <label className="text-white" htmlFor="email">
             Email
           </label>
           <input
-            className="w-60 rounded-full shadow-md"
+            className="w-80 rounded-lg shadow-md"
             type="email"
             name="email"
+            onChange={formik.handleChange}
+            value={formik.values.email}
           ></input>
-          <label className="mx-5" htmlFor="password">
+          <label className="text-white" htmlFor="password">
             Password
           </label>
           <input
-            className="w-60 rounded-full shadow-md"
+            className="w-80 rounded-lg shadow-md"
             type="password"
             name="password"
+            onChange={formik.handleChange}
+            value={formik.values.password}
           ></input>
-          <label className="mx-5" htmlFor="currentJob">
+          <label className="text-white" htmlFor="currentJob">
             Current Job
           </label>
           <input
-            className="w-60 rounded-full shadow-md"
+            className="w-80 rounded-lg shadow-md"
             type="text"
             name="currentJob"
+            onChange={formik.handleChange}
+            value={formik.values.currentJob}
           ></input>
-          <label className="mx-5" htmlFor="description">
+          <label className="text-white" htmlFor="description">
             Description
           </label>
           <textarea
-            className="w-60 rounded-lg shadow-md"
+            className="w-80 rounded-lg shadow-md"
             type="text"
             name="description"
-            rows="4"
-            cols="50"
+            onChange={formik.handleChange}
+            value={formik.values.description}
           ></textarea>
-          <button type="submit">Submit</button>
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded my-2"
+            type="submit"
+          >
+            Submit
+          </button>
         </form>
       </div>
     </div>
