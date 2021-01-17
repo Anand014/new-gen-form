29d3454a02df074ddd77bfc615de74b3367efe73
Anand014 <kumar.anand014@gmail.com>
2021-01-17 18:01:30 +0530

    added logic and winRate as well as gem multiplier

--- frontend/src/Components/Multiplygems/Multiplygems.js
+++ frontend/src/Components/Multiplygems/Multiplygems.js
@@ -1,52 +1,183 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import purple from "../../assets/purple.svg";
 import crystal from "../../assets/crystal.svg";
+import "./Multiplygems.css";
 
 const Multiplygems = () => {
+  const [addGems, setAddGems] = useState("");
+  const [multiplier, setMultiplier] = useState("");
+  const [winRate, setWinRate] = useState(0);
+
+  useEffect(() => {
+    if (multiplier !== "") {
+      let percent;
+      percent = (addGems * 100) / multiplier;
+      setWinRate(percent);
+      console.log(percent);
+    }
+  }, [multiplier]);
+
+  const handleTen = () => {
+    setAddGems(10);
+  };
+  const handleTwenty = () => {
+    setAddGems(20);
+  };
+  const handleFifty = () => {
+    setAddGems(50);
+  };
+  const handleHundred = () => {
+    setAddGems(100);
+  };
+  const handleTwoHundred = () => {
+    setAddGems(200);
+  };
+  const handleMultiplierone = () => {
+    if (addGems !== "") {
+      let multiply;
+      multiply = 1.5 * addGems;
+      setMultiplier(multiply);
+    }
+  };
+  const handleMultiplierTwo = () => {
+    if (addGems !== "") {
+      let multiply;
+      multiply = 2 * addGems;
+      setMultiplier(multiply);
+    }
+  };
+  const handleMultiplierFive = () => {
+    if (addGems !== "") {
+      let multiply;
+      multiply = 5 * addGems;
+      setMultiplier(multiply);
+    }
+  };
+  const handleMultiplierTen = () => {
+    if (addGems !== "") {
+      let multiply;
+      multiply = 10 * addGems;
+      setMultiplier(multiply);
+    }
+  };
+  const handleMultiplierTwenty = () => {
+    if (addGems !== "") {
+      let multiply;
+      multiply = 20 * addGems;
+      setMultiplier(multiply);
+    }
+  };
+
   return (
     <div className="flex justify-center">
       <div className="glasseffectuser w-4/5 p-5">
-        <div className="grid grid-cols-1 md:grid-cols-3">
+        <div className="grid grid-cols-1 lg:grid-cols-3">
           <div className="rounded bg-yellow-50 shadow-lg bg-opacity-50">
             <div class="grid justify-items-center">
               <h1 className="text-2xl font-bold my-2 formtext">Your Gems</h1>
               <img className="w-1/3" src={purple} />
-              <div className="mt-6">
+              <div className="mt-2">
                 <span className="z-10 h-full leading-snug font-normal absolute absolute items-center justify-center w-8 pl-3 py-3">
                   <img src={purple} />
                 </span>
                 <input
                   type="number"
+                  value={addGems}
                   placeholder="Add Gems"
                   className="px-3 py-3 placeholder-gray-400 text-gray-700 relative bg-white rounded text-sm border border-gray-400 outline-none focus:outline-none focus:shadow-outline pl-10"
+                  onChange={(e) => setAddGems(e.target.value)}
                 />
+              </div>
+              <div class="flex flex-row mt-2">
+                <button
+                  className="bg-blue-500 hover:bg-blue-700 text-white font-bold p-2 m-2 rounded"
+                  onClick={handleTen}
+                >
+                  10
+                </button>
+                <button
+                  className="bg-blue-500 hover:bg-blue-700 text-white font-bold p-2 m-2 rounded"
+                  onClick={handleTwenty}
+                >
+                  20
+                </button>
+                <button
+                  className="bg-blue-500 hover:bg-blue-700 text-white font-bold p-2 m-2 rounded"
+                  onClick={handleFifty}
+                >
+                  50
+                </button>
+                <button
+                  className="bg-blue-500 hover:bg-blue-700 text-white font-bold p-2 m-2 rounded"
+                  onClick={handleHundred}
+                >
+                  100
+                </button>
+                <button
+                  className="bg-blue-500 hover:bg-blue-700 text-white font-bold p-2 m-2 rounded"
+                  onClick={handleTwoHundred}
+                >
+                  200
+                </button>
               </div>
             </div>
           </div>
-          <div className="w-3/4 rounded bg-yellow-50 bg-opacity-50 shadow-lg justify-self-center">
+          <div className="w-3/4 rounded bg-yellow-50 bg-opacity-50 shadow-lg justify-self-center m-2 midbox">
             <div class="grid justify-items-center">
-              <div> hello</div>
+              <h1 className="text-xl formtext font-black text-white my-2 mt-32 mb-32 z-10">
+                {winRate}%
+              </h1>
             </div>
           </div>
           <div className="rounded bg-yellow-50 bg-opacity-50 shadow-lg">
             <div class="grid justify-items-center">
               <h1 className="text-2xl font-bold my-2 formtext">Upgrade</h1>
               <img className="w-1/3" src={crystal} />
-              <h4 className="text-lg font-bold my-2 formtext">200</h4>
-              <div class="flex flex-col md:flex-row">
-                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold p-2 m-2 rounded">
+              {/* <div className="grid grid-cols-2 justify-items-center mt-2"> */}
+              {/* <input
+                  type="number"
+                  value={multiplier}
+                  placeholder="Win Gems"
+                  className="w-3/4 justify-self-end text-center placeholder-gray-400 text-gray-700 relative bg-white rounded text-sm border border-gray-400 outline-none focus:outline-none focus:shadow-outline mr-1"
+                  onChange={(e) => setMultiplier(e.target.value)}
+                /> */}
+              <div className="bg-yellow-50 px-4 rounded mt-1">
+                {" "}
+                {/* justify-self-start ml-5 */}
+                <h4 className="text-lg font-bold my-2 formtext">
+                  {multiplier ? multiplier : "0"}
+                </h4>
+              </div>
+              {/* </div> */}
+              <div class="flex flex-row mt-2">
+                <button
+                  className="bg-blue-500 hover:bg-blue-700 text-white font-bold p-2 m-2 rounded"
+                  onClick={handleMultiplierone}
+                >
                   1.5x
                 </button>
-                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold p-2 m-2 rounded">
+                <button
+                  className="bg-blue-500 hover:bg-blue-700 text-white font-bold p-2 m-2 rounded"
+                  onClick={handleMultiplierTwo}
+                >
                   2x
                 </button>
-                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold p-2 m-2 rounded">
+                <button
+                  className="bg-blue-500 hover:bg-blue-700 text-white font-bold p-2 m-2 rounded"
+                  onClick={handleMultiplierFive}
+                >
                   5x
                 </button>
-                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold p-2 m-2 rounded">
+                <button
+                  className="bg-blue-500 hover:bg-blue-700 text-white font-bold p-2 m-2 rounded"
+                  onClick={handleMultiplierTen}
+                >
                   10x
                 </button>
-                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold p-2 m-2 rounded">
+                <button
+                  className="bg-blue-500 hover:bg-blue-700 text-white font-bold p-2 m-2 rounded"
+                  onClick={handleMultiplierTwenty}
+                >
                   20x
                 </button>
               </div>
